e75e3f722b1815896f80816e5c43f4b0b82b7cbf
Giovanna <Giovanna8@users.noreply.github.com>
2016-11-15 22:43:11 -0600

    Update to app.js
    Line 6 and 7: app.use  !app.user

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.user(bodyParser.json());
-app.user(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = 3000;
 app.listen(port, () => console.log(`Listening on ${port}`));
